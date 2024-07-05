d2bc7801e8d945e01a6785f214661a258c7caf6a
Arul-AM <arulak220@gmail.com>
2024-07-05 13:45:44 +0530

    01

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to apply animation to background images in .home section
+document.addEventListener('DOMContentLoaded', function() {
+    const homeSection = document.querySelector('.home');
+    homeSection.style.animation = 'changeBackground 20s infinite';
+});
